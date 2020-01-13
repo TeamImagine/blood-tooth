51ee70b1b486ceb041445e113210e1d66a220c7a
murph <murphyl@outlook.com>
2020-01-13 20:17:11 +0800

    敲定引擎，尝试编码

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import pyglet
+
+window = pyglet.window.Window(caption = '血牙')
+
+pic = pyglet.image.load('./assets/fire.gif')
+
+window.set_icon(pic)
+
+@window.event
+def on_draw():
+    window.clear()
+
+pyglet.app.run()
